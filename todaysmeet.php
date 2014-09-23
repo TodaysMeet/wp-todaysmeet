bbc99981674344a092e964f946f4c13331949988
James Socol <me@jamessocol.com>
2014-09-23 11:26:45 -0400

    Forget 1.0.3, 1.0.4 FTW

--- todaysmeet.php
+++ todaysmeet.php
@@ -3,7 +3,7 @@
  * Plugin Name: TodaysMeet
  * Plugin URI: * https://todaysmeet.com/help/faq/14
  * Description: Embed TodaysMeet rooms in your posts and pages.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: TodaysMeet
  * Author URI: https://todaysmeet.com/
  * License: GPL2
